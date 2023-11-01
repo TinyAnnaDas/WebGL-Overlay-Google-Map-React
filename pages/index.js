fc6e427273a06e18031c749e9f3cb153a2bf2ef0
Tiny P Christhudas <tinypchristhudas@gmail.com>
2023-11-01 09:35:38 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function Home (){
+
+  const {} =  useLoadScript({googleMapApiKey : ""})
+
+  return <div>Map</div>
+}
